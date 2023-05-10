eb8267d8f2c3f5795b2cbe6b20cb3286226bd0d6
jinal30 <jinal30.sangani@gmail.com>
2023-05-10 18:31:25 +0530

    product page

--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -11,18 +11,3 @@
 document.querySelector(".js-qty__adjust").addEventListener('click', () => {
     alert('hello');
 });
-
-function increaseValue() {
-    var value = parseInt(document.getElementsByClassName('js-qty__num').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementsByClassName('js-qty__num').value = value;
-}
-
-function decreaseValue() {
-    var value = parseInt(document.getElementsByClassName('js-qty__num').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value < 1 ? value = 1 : '';
-    value--;
-    document.getElementsByClassName('js-qty__num').value = value;
-}
