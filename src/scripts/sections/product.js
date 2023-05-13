3d5ab6a0e7e0a9d3c2d29c4ad9f16513cbaf075c
jinal30 <jinal30.sangani@gmail.com>
2023-05-13 18:24:44 +0530

    product page

--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -11,3 +11,28 @@
 document.querySelector(".js-qty__adjust").addEventListener('click', () => {
     alert('hello');
 });
+
+// description according dropdown menu 
+const accordionTitles = document.querySelectorAll(".accordionTitle");
+
+accordionTitles.forEach((accordionTitle) => {
+    accordionTitle.addEventListener("click", () => {
+        if (accordionTitle.classList.contains("is-open")) {
+            accordionTitle.classList.remove("is-open");
+        } else {
+            const accordionTitlesWithIsOpen = document.querySelectorAll(".is-open");
+            accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
+                accordionTitleWithIsOpen.classList.remove("is-open");
+            });
+            accordionTitle.classList.add("is-open");
+        }
+    });
+});
+
+//product image silder
+var mainSlider = new Flickity('.product-image', {
+    autoPlay: true,
+    pageDots: true,
+    prevNextButtons: true,
+    wrapAround: true
+});
